b1ee0e9ea3f8a7ddb49cc1d9c9d612a6b2d71158
Yaniv Siboni <yaniv.siboni@post.runi.ac.il>
2025-11-08 22:50:50 +0200

    DONE

--- Cheers.java
+++ Cheers.java
@@ -1,30 +1,19 @@
-//feedback
 
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
 	String userName = args[0];
         int userNumber = Integer.parseInt(args[1]);
-        int index = 0;
         String anChars = "aefhilmnorsxAEFHILMNORSX";
-        int count = 0;
-        while (count < userNumber + 1){
-                char c = userName.charAt(index);
+        for (int count = 0; count < userName.length(); count++){
+                char c = userName.charAt(count);
                 if (anChars.indexOf(c) == -1){
                 System.out.println("Give me a  " + Character.toUpperCase(c) + ": " + Character.toUpperCase(c) + "!");      
                 } else {
                         System.out.println("Give me an  " + Character.toUpperCase(c) + ": " + Character.toUpperCase(c) + "!");
                 }
-                count++;
-                index++;
                 }
                 System.out.println("What does that spell?");
-                for (int a = 0; a < userNumber + 1; a++){
+                for (int a = 0; a < userNumber; a++){
                         System.out.println(userName.toUpperCase() + "!!!");
                 }
         }
