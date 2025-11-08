6ba23d79cd4ad72ba59faf67063818eeff450702
Yaniv Siboni <yaniv.siboni@post.runi.ac.il>
2025-11-08 22:17:32 +0200

    check

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,25 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	String userName = args[0];
+        int userNumber = Integer.parseInt(args[1]);
+        int index = 0;
+        String anChars = "aefhilmnorsxAEFHILMNORSX";
+        int count = 0;
+        while (count < userNumber + 1){
+                char c = userName.charAt(index);
+                if (anChars.indexOf(c) == -1){
+                System.out.println("Give me a " + Character.toUpperCase(c) + ": " + Character.toUpperCase(c) + "!");      
+                } else {
+                        System.out.println("Give me an " + Character.toUpperCase(c) + ": " + Character.toUpperCase(c) + "!");
+                }
+                count++;
+                index++;
+                }
+                System.out.println("What does that spell?");
+                for (int a = 0; a < userNumber + 1; a++){
+                        System.out.println(userName.toUpperCase() + "!!!");
+                }
         }
-}
+        }
+
