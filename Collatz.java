e1032c0cc8dbef09a3c3928235e4840065a353e6
Yaniv Siboni <yaniv.siboni@post.runi.ac.il>
2025-11-09 01:00:26 +0200

    done

--- Collatz.java
+++ Collatz.java
@@ -12,7 +12,7 @@ public class Collatz {
 	System.out.print(number);
 		}
 		
-		while (number != 1){
+		while (number != 1 || steps == 1){
 			if (number % 2 == 0) {
 				number = number / 2;
 			} else {
